67622253c7e731f17a5c687f88380e43e649624a
AnikKDev <anikdev2016@gmail.com>
2022-07-13 17:06:22 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DB_PASS}@cluster0.kstuc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const usersCollection = client.db('vitaradb').collection('usersCollection');
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
